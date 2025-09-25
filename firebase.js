01e2ed43d1b39d2b8148c895b2d5998546b8c848
joiceestevao <joiceestevao2514@gmail.com>
2025-09-25 20:49:07 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,20 @@
-export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJECT_ID.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT_ID.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SUA_APP_ID"
+// Importa os SDKs necessários
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
+// Configuração do Firebase (a sua)
+const firebaseConfig = {
+  apiKey: "AIzaSyCyCnlYITfYUJxVMl6eYf-tS6FCuZZckG8",
+  authDomain: "posto-de-saude-f909a.firebaseapp.com",
+  projectId: "posto-de-saude-f909a",
+  storageBucket: "posto-de-saude-f909a.firebasestorage.app",
+  messagingSenderId: "174308089289",
+  appId: "1:174308089289:web:86c21059e431b987fd5c18",
+  measurementId: "G-4QSM5E9GMR"
 };
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta o Firestore para usar em app.js
+export const db = getFirestore(app);
